d8c91638c8ad6df1e41157344fecaa5fbc6a2e2e
Frommi <daniil.liferenko@gmail.com>
2017-05-15 21:13:04 +0300

    no error on create_project

--- Bot/project_func.py
+++ Bot/project_func.py
@@ -1,40 +1,39 @@
 import BD
 import answers
 import re
-from util import get_chat_id, get_user_id, too_few_args, cursor, \
-    GroupNotExistsException, ProjectNotExistsException, ProjectAlreadyExistsException, UserIsNotAdmin
+import util
 from telegram.ext import CommandHandler
 
 #args - group_name, project_name
 def create_project(bot, update, args):
-    if too_few_args(bot, get_chat_id(update), args, 2) is None:
+    if util.too_few_args(bot, util.get_chat_id(update), args, 2) is None:
         return
     if re.fullmatch("\w+", args[0]) is None:
-        bot.send_message(chat_id=get_chat_id(update), text=answers.incorrect_name + "(группы)")
+        bot.send_message(chat_id=util.get_chat_id(update), text=answers.incorrect_name + "(группы)")
         return
     if re.fullmatch("\w+", args[1]) is None:
-        bot.send_message(chat_id=get_chat_id(update), text=answers.incorrect_name + "(проекта)")
+        bot.send_message(chat_id=util.get_chat_id(update), text=answers.incorrect_name + "(проекта)")
         return
     try:
-        BD.create_project(cursor, get_user_id(update), args[0], args[1])
-    except ProjectAlreadyExistsException:
-        bot.send_message(chat_id=get_chat_id(update), ext=answers.already_exists_name % "проект")
-    except UserIsNotAdmin:
-        bot.send_message(chat_id=get_chat_id(update), text=answers.create_no_admin % "проект")
+        BD.create_project(util.cursor, util.get_user_id(update), args[1], args[0])
+    except util.ProjectAlreadyExistsException:
+        bot.send_message(chat_id=util.get_chat_id(update), ext=answers.already_exists_name % "проект")
+    except util.UserIsNotAdmin:
+        bot.send_message(chat_id=util.get_chat_id(update), text=answers.create_no_admin % "проект")
 
 #args = project_name
 def delete_project(bot, update, args):
-    if too_few_args(bot, get_chat_id(update), args, 1) is None:
+    if util.too_few_args(bot, util.get_chat_id(update), args, 1) is None:
         return
     if re.fullmatch("\w+", args[0]) is None:
-        bot.send_message(chat_id=get_chat_id(update), text=answers.incorrect_name + "(проекта)")
+        bot.send_message(chat_id=util.get_chat_id(update), text=answers.incorrect_name + "(проекта)")
         return
     try:
-        BD.delete_project(cursor, get_user_id(update), args[0])
-    except ProjectNotExistsException:
-        bot.send_message(chat_id=get_chat_id(update),text=answers.no_exists_name % "проект")
-    except UserIsNotAdmin:
-        bot.send_message(chat_id=get_chat_id(update),text=answers.delete_no_admin % "проект")
+        BD.delete_project(util.cursor, util.get_user_id(update), args[0])
+    except util.ProjectNotExistsException:
+        bot.send_message(chat_id=util.get_chat_id(update),text=answers.no_exists_name % "проект")
+    except util.UserIsNotAdmin:
+        bot.send_message(chat_id=util.get_chat_id(update),text=answers.delete_no_admin % "проект")
 
 # def project_info(bot, update, user_id, string_name):
 #     pass
--- Bot/task_func.py
+++ Bot/task_func.py
@@ -2,141 +2,140 @@ import BD
 import answers
 import re
 from telegram.ext import CommandHandler
-from util import get_chat_id, get_user_id, too_few_args, cursor, \
-    ProjectNotExistsException, TaskNotExistsException, TaskAlreadyExistsException, \
-    UserIsNotAdmin, UserHasNotTask
+import util
+
 
 #args - project_name, task_name
 def create_task(bot, update, args):
-    if too_few_args(bot, get_chat_id(update), args, 2) is None:
+    if util.too_few_args(bot, util.get_chat_id(update), args, 2) is None:
         return
     names = ("(проекта)", "(таска)")
     for i in range(2):
         if re.fullmatch("\w+", args[i]) is None:
-            bot.send_message(chat_id=get_chat_id(update), text=answers.incorrect_name + names[i])
+            bot.send_message(chat_id=util.get_chat_id(update), text=answers.incorrect_name + names[i])
             return
     try:
-        BD.create_task(cursor, get_user_id(update), args[1], args[0])
-    except ProjectNotExistsException:
-        bot.send_message(chat_id=get_chat_id(update), text=answers.no_exists_name % "проект")
-    except TaskAlreadyExistsException:
-        bot.send_message(chat_id=get_chat_id(update), text=answers.already_exists_name % "таск")
-    except UserIsNotAdmin:
-        bot.send_message(chat_id=get_chat_id(update), text=answers.create_no_admin % "таск")
+        BD.create_task(util.cursor, util.get_user_id(update), args[1], args[0])
+    except util.ProjectNotExistsException:
+        bot.send_message(chat_id=util.get_chat_id(update), text=answers.no_exists_name % "проект")
+    except util.TaskAlreadyExistsException:
+        bot.send_message(chat_id=util.get_chat_id(update), text=answers.already_exists_name % "таск")
+    except util.UserIsNotAdmin:
+        bot.send_message(chat_id=util.get_chat_id(update), text=answers.create_no_admin % "таск")
 
 #args - project_name, task_name
 def delete_task(bot, update, args):
-    if too_few_args(bot, get_chat_id(update), args, 2) is None:
+    if util.too_few_args(bot, util.get_chat_id(update), args, 2) is None:
         return
     names = ("(проекта)", "(таска)")
     for i in range(2):
         if re.fullmatch("\w+", args[i]) is None:
-            bot.send_message(chat_id=get_chat_id(update), text=answers.incorrect_name + names[i])
+            bot.send_message(chat_id=util.get_chat_id(update), text=answers.incorrect_name + names[i])
             return
     try:
-        BD.delete_task(cursor, get_user_id(update), args[1])#,args[0])
-    except ProjectNotExistsException:
-        bot.send_message(chat_id=get_chat_id(update), text=answers.no_exists_name % "проект")
-    except TaskNotExistsException:
-        bot.send_message(chat_id=get_chat_id(update), text=answers.already_exists_name % "таск")
-    except UserIsNotAdmin:
-        bot.send_message(chat_id=get_chat_id(update), text=answers.create_no_admin % "таск")
+        BD.delete_task(util.cursor, util.get_user_id(update), args[1])#,args[0])
+    except util.ProjectNotExistsException:
+        bot.send_message(chat_id=util.get_chat_id(update), text=answers.no_exists_name % "проект")
+    except util.TaskNotExistsException:
+        bot.send_message(chat_id=util.get_chat_id(update), text=answers.already_exists_name % "таск")
+    except util.UserIsNotAdmin:
+        bot.send_message(chat_id=util.get_chat_id(update), text=answers.create_no_admin % "таск")
 
 #args - project_name, task_name
 def done_task(bot, update, args):
-    if too_few_args(bot, get_chat_id(update), args, 2) is None:
+    if util.too_few_args(bot, util.get_chat_id(update), args, 2) is None:
         return
     names = ("(проекта)", "(таска)")
     for i in range(2):
         if re.fullmatch("\w+", args[i]) is None:
-            bot.send_message(chat_id=get_chat_id(update), text=answers.incorrect_name + names[i])
+            bot.send_message(chat_id=util.get_chat_id(update), text=answers.incorrect_name + names[i])
             return
     try:
-        BD.set_task(cursor, get_user_id(update), args[1], True, None)#,args[0])
-    except ProjectNotExistsException:
-        bot.send_message(chat_id=get_chat_id(update), text=answers.no_exists_name % "проект")
-    except TaskNotExistsException:
-        bot.send_message(chat_id=get_chat_id(update), text=answers.already_exists_name % "таск")
-    except UserHasNotTask:
-        bot.send_message(chat_id=get_chat_id(update), text=answers.create_no_admin % "таск")
+        BD.set_task(util.cursor, util.get_user_id(update), args[1], True, None)#,args[0])
+    except util.ProjectNotExistsException:
+        bot.send_message(chat_id=util.get_chat_id(update), text=answers.no_exists_name % "проект")
+    except util.TaskNotExistsException:
+        bot.send_message(chat_id=util.get_chat_id(update), text=answers.already_exists_name % "таск")
+    except util.UserHasNotTask:
+        bot.send_message(chat_id=util.get_chat_id(update), text=answers.create_no_admin % "таск")
 
 
 #args - project_name, task_name
 def undone_task(bot, update, args):
-    if too_few_args(bot, get_chat_id(update), args, 2) is None:
+    if util.too_few_args(bot, util.get_chat_id(update), args, 2) is None:
         return
     names = ("(проекта)", "(таска)")
     for i in range(2):
         if re.fullmatch("\w+", args[i]) is None:
-            bot.send_message(chat_id=get_chat_id(update), text=answers.incorrect_name + names[i])
+            bot.send_message(chat_id=util.get_chat_id(update), text=answers.incorrect_name + names[i])
             return
     try:
-        BD.set_task(cursor, get_user_id(update), args[1], False, None)#,args[0])
-    except ProjectNotExistsException:
-        bot.send_message(chat_id=get_chat_id(update), text=answers.no_exists_name % "проект")
-    except TaskNotExistsException:
-        bot.send_message(chat_id=get_chat_id(update), text=answers.already_exists_name % "таск")
-    except UserHasNotTask:
-        bot.send_message(chat_id=get_chat_id(update), text=answers.create_no_admin % "таск")
+        BD.set_task(util.cursor, util.get_user_id(update), args[1], False, None)#,args[0])
+    except util.ProjectNotExistsException:
+        bot.send_message(chat_id=util.get_chat_id(update), text=answers.no_exists_name % "проект")
+    except util.TaskNotExistsException:
+        bot.send_message(chat_id=util.get_chat_id(update), text=answers.already_exists_name % "таск")
+    except util.UserHasNotTask:
+        bot.send_message(chat_id=util.get_chat_id(update), text=answers.create_no_admin % "таск")
 
 
 #args - project_name, task_name
 def hide_task(bot, update, args):
-    if too_few_args(bot, get_chat_id(update), args, 2) is None:
+    if util.too_few_args(bot, util.get_chat_id(update), args, 2) is None:
         return
     names = ("(проекта)", "(таска)")
     for i in range(2):
         if re.fullmatch("\w+", args[i]) is None:
-            bot.send_message(chat_id=get_chat_id(update), text=answers.incorrect_name + names[i])
+            bot.send_message(chat_id=util.get_chat_id(update), text=answers.incorrect_name + names[i])
             return
     try:
-        BD.set_task(cursor, get_user_id(update), args[1], None, True)#,args[0])
-    except ProjectNotExistsException:
-        bot.send_message(chat_id=get_chat_id(update), text=answers.no_exists_name % "проект")
-    except TaskNotExistsException:
-        bot.send_message(chat_id=get_chat_id(update), text=answers.already_exists_name % "таск")
-    except UserHasNotTask:
-        bot.send_message(chat_id=get_chat_id(update), text=answers.create_no_admin % "таск")
+        BD.set_task(util.cursor, util.get_user_id(update), args[1], None, True)#,args[0])
+    except util.ProjectNotExistsException:
+        bot.send_message(chat_id=util.get_chat_id(update), text=answers.no_exists_name % "проект")
+    except util.TaskNotExistsException:
+        bot.send_message(chat_id=util.get_chat_id(update), text=answers.already_exists_name % "таск")
+    except util.UserHasNotTask:
+        bot.send_message(chat_id=util.get_chat_id(update), text=answers.create_no_admin % "таск")
 
 #args - project_name, task_name
 def unhide_task(bot, update, args):
-    if too_few_args(bot, get_chat_id(update), args, 2) is None:
+    if util.too_few_args(bot, util.get_chat_id(update), args, 2) is None:
         return
     names = ("(проекта)", "(таска)")
     for i in range(2):
         if re.fullmatch("\w+", args[i]) is None:
-            bot.send_message(chat_id=get_chat_id(update), text=answers.incorrect_name + names[i])
+            bot.send_message(chat_id=util.get_chat_id(update), text=answers.incorrect_name + names[i])
             return
     try:
-        BD.set_task(cursor, get_user_id(update), args[1], None, False)#,args[0])
-    except ProjectNotExistsException:
-        bot.send_message(chat_id=get_chat_id(update), text=answers.no_exists_name % "проект")
-    except TaskNotExistsException:
-        bot.send_message(chat_id=get_chat_id(update), text=answers.already_exists_name % "таск")
-    except UserHasNotTask:
-        bot.send_message(chat_id=get_chat_id(update), text=answers.create_no_admin % "таск")
+        BD.set_task(util.cursor, util.get_user_id(update), args[1], None, False)#,args[0])
+    except util.ProjectNotExistsException:
+        bot.send_message(chat_id=util.get_chat_id(update), text=answers.no_exists_name % "проект")
+    except util.TaskNotExistsException:
+        bot.send_message(chat_id=util.get_chat_id(update), text=answers.already_exists_name % "таск")
+    except util.UserHasNotTask:
+        bot.send_message(chat_id=util.get_chat_id(update), text=answers.create_no_admin % "таск")
 
 #task_status d - 'done', u - 'undone'
 #args - project_name, task_name1, task_status1, task_name2, task_status2,...
 def find_botan(bot, update, args):
     if len(args) % 2 == 0:
-        bot.send_message(chat_id=get_chat_id(update), text=answers.incorrect_input_tasks)
+        bot.send_message(chat_id=util.get_chat_id(update), text=answers.incorrect_input_tasks)
         return
     for i in range(1, len(args), 2):
         if args[i] != 'u' and args[i] != 'd':
-            bot.send_message(chat_id=get_chat_id(update), text=answers.incorrect_input_tasks)
+            bot.send_message(chat_id=util.get_chat_id(update), text=answers.incorrect_input_tasks)
 
     tasks = [(args[i], args[i + 1] == 'd') for i in range(1, len(args), 2)]
 
     try:
-        botans = BD.find_botan(cursor, tasks)#,args[0])
-        bot.send_message(chat_id=get_chat_id(update), text='\n'.join(botans))
-    except ProjectNotExistsException:
-        bot.send_message(chat_id=get_chat_id(update), text=answers.no_exists_name % "проект")
-    except TaskNotExistsException:
-        bot.send_message(chat_id=get_chat_id(update), text=answers.already_exists_name % "таск")
-    except UserHasNotTask:
-        bot.send_message(chat_id=get_chat_id(update), text=answers.create_no_admin % "таск")
+        botans = BD.find_botan(util.cursor, tasks)#,args[0])
+        bot.send_message(chat_id=util.get_chat_id(update), text='\n'.join(botans))
+    except util.ProjectNotExistsException:
+        bot.send_message(chat_id=util.get_chat_id(update), text=answers.no_exists_name % "проект")
+    except util.TaskNotExistsException:
+        bot.send_message(chat_id=util.get_chat_id(update), text=answers.already_exists_name % "таск")
+    except util.UserHasNotTask:
+        bot.send_message(chat_id=util.get_chat_id(update), text=answers.create_no_admin % "таск")
 
 
 task_handlers = (
