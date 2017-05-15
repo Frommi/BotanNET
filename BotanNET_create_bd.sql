80ba910a5bb4c6cdd67ae74137d575f205772f2f
Frommi <daniil.liferenko@gmail.com>
2017-05-15 04:22:26 +0300

    BD functions added, not tested

--- BotanNET_create_bd.sql
+++ BotanNET_create_bd.sql
@@ -1,43 +1,39 @@
-CREATE TABLE Tasks (
+CREATE TABLE tasks (
     "task_id" serial NOT NULL,
     "task_info" TEXT NOT NULL,
-    CONSTRAINT Tasks_pk PRIMARY KEY ("task_id")
+    CONSTRAINT tasks_pk PRIMARY KEY ("task_id")
 ) WITH (
     OIDS=FALSE
 );
 
-CREATE TABLE Projects (
+CREATE TABLE projects (
     "project_id" serial NOT NULL,
+    "project_name" varchar(50) NOT NULL,
     "group_id" integer NOT NULL,
-    "project_name" varchar(50) NOT NULL DEFAULT 'default',
     "project_info" TEXT DEFAULT NULL,
     "deadline" TIMESTAMP DEFAULT NULL,
-    CONSTRAINT Projects_pk PRIMARY KEY ("project_id")
+    CONSTRAINT projects_pk PRIMARY KEY ("project_id")
 ) WITH (
     OIDS=FALSE
 );
 
-CREATE TABLE Users (
-    "user_id" serial NOT NULL,
-    "user_name" varchar(50) NOT NULL,
-    "deparment" varchar(30) NOT NULL,
-    "dorm_number" integer NOT NULL,
-    "group_number" integer NOT NULL,
-    CONSTRAINT Users_pk PRIMARY KEY ("user_id")
+CREATE TABLE users (
+    "user_id" bigint NOT NULL,
+    CONSTRAINT users_pk PRIMARY KEY ("user_id")
 ) WITH (
     OIDS=FALSE
 );
 
-CREATE TABLE Groups (
+CREATE TABLE groups (
     "group_id" serial NOT NULL,
     "group_name" varchar(50) NOT NULL,
-    "teacher_name" varchar(100),
-    CONSTRAINT Groups_pk PRIMARY KEY ("group_id")
+    "group_info" TEXT DEFAULT NULL,
+    CONSTRAINT groups_pk PRIMARY KEY ("group_id")
 ) WITH (
     OIDS=FALSE
 );
 
-CREATE TABLE UserGroups (
+CREATE TABLE user_groups (
     "group_id" integer NOT NULL,
     "user_id" integer NOT NULL,
     "is_admin" bool NOT NULL
@@ -45,14 +41,14 @@ CREATE TABLE UserGroups (
     OIDS=FALSE
 );
 
-CREATE TABLE ProjectTasks (
+CREATE TABLE project_tasks (
     "project_id" integer NOT NULL,
     "task_id" integer NOT NULL
 ) WITH (
     OIDS=FALSE
 );
 
-CREATE TABLE UserTasks (
+CREATE TABLE user_tasks (
     "user_id" integer NOT NULL,
     "task_id" integer NOT NULL,
     "is_hidden" bool NOT NULL DEFAULT 'false',
@@ -61,32 +57,17 @@ CREATE TABLE UserTasks (
     OIDS=FALSE
 );
 
-CREATE TABLE Comments (
-    "task_id" integer NOT NULL,
-    "comment_no" integer NOT NULL,
-    "user_id" integer NOT NULL,
-    "comment_text" TEXT NOT NULL
-) WITH (
-    OIDS=FALSE
-);
+
+ALTER TABLE projects ADD CONSTRAINT "projects_fk0" FOREIGN KEY ("group_id") REFERENCES groups("group_id");
+
+ALTER TABLE user_groups ADD CONSTRAINT "user_groups_fk0" FOREIGN KEY ("group_id") REFERENCES groups("group_id");
+ALTER TABLE user_groups ADD CONSTRAINT "user_groups_fk1" FOREIGN KEY ("user_id") REFERENCES users("user_id");
+
+ALTER TABLE project_tasks ADD CONSTRAINT "project_tasks_fk0" FOREIGN KEY ("project_id") REFERENCES projects("project_id");
+ALTER TABLE project_tasks ADD CONSTRAINT "project_tasks_fk1" FOREIGN KEY ("task_id") REFERENCES tasks("task_id");
+
+ALTER TABLE user_tasks ADD CONSTRAINT "user_tasks_fk0" FOREIGN KEY ("user_id") REFERENCES users("user_id");
+ALTER TABLE user_tasks ADD CONSTRAINT "user_tasks_fk1" FOREIGN KEY ("task_id") REFERENCES tasks("task_id");
 
 
-ALTER TABLE Projects ADD CONSTRAINT "Projects_fk0" FOREIGN KEY ("group_id") REFERENCES Groups("group_id");
-
-ALTER TABLE UserGroups ADD CONSTRAINT "UserGroups_fk0" FOREIGN KEY ("group_id") REFERENCES Groups("group_id");
-ALTER TABLE UserGroups ADD CONSTRAINT "UserGroups_fk1" FOREIGN KEY ("user_id") REFERENCES Users("user_id");
-
-ALTER TABLE ProjectTasks ADD CONSTRAINT "ProjectTasks_fk0" FOREIGN KEY ("project_id") REFERENCES Projects("project_id");
-ALTER TABLE ProjectTasks ADD CONSTRAINT "ProjectTasks_fk1" FOREIGN KEY ("task_id") REFERENCES Tasks("task_id");
-
-ALTER TABLE UserTasks ADD CONSTRAINT "UserTasks_fk0" FOREIGN KEY ("user_id") REFERENCES Users("user_id");
-ALTER TABLE UserTasks ADD CONSTRAINT "UserTasks_fk1" FOREIGN KEY ("task_id") REFERENCES Tasks("task_id");
-
-ALTER TABLE Comments ADD CONSTRAINT "Comments_fk0" FOREIGN KEY ("task_id") REFERENCES Tasks("task_id");
-ALTER TABLE Comments ADD CONSTRAINT "Comments_fk1" FOREIGN KEY ("user_id") REFERENCES Users("user_id");
-
-
-CREATE INDEX deadline_index ON Projects(deadline);
-
-CREATE INDEX dorm_number_index ON Users(dorm_number);
-CREATE INDEX group_number_index ON Users(group_number);
+CREATE INDEX deadline_index ON projects(deadline);
